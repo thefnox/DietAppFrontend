63713b6f21f74fbd44326b960de7ac4ec31efd39
fnox <thefnox@gmail.com>
2019-04-15 10:31:23 +0200

    Initial commit

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,10 +1,14 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'
+import DiaryList from '../Containers/DiaryList'
+import LoginScreen from '../Containers/LoginScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = createStackNavigator({
+  DiaryList: { screen: DiaryList },
+  LoginScreen: { screen: LoginScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
